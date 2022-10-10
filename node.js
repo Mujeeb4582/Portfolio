04f6c91a7c75cac42274d1b2b3fd98040df9dec2
ginabeki <ginabeki3@gmail.com>
2022-10-10 15:31:40 +0300

    Added toggle functionality to the menu item

--- node.js
+++ node.js
@@ -1,20 +1,23 @@
-const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menuItem');
-const hamburger= document.querySelector('.hamburger');
-const closeIcon= document.querySelector('.closeIcon');
-const menuIcon = document.querySelector('.menuIcon');
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
 function toggleMenu() {
-  if (menu.classList.contains('showMenu')) {
-    menu.classList.remove('showMenu');
-    closeIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
   } else {
-    menu.classList.add('showMenu');
-    closeIcon.style.display = 'block';
-    menuIcon.style.display = 'none';
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
   }
 }
 
-hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
 
+menuItems.forEach(function (menuItem) {
+  menuItem.addEventListener("click", toggleMenu);
+});
