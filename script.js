d1dd2d4649e55e690c91bc9cd5a96fd5a5cbc826
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-11 23:28:32 +0500

    Add the media quary and dynamic code for desktop

--- script.js
+++ script.js
@@ -22,14 +22,20 @@ menuItems.forEach((menuItem) => {
   menuItem.addEventListener('click', toggleMenu);
 });
 
-// --------POPUP Window
+// media quary -------------------
 
-const body = document.querySelector('body');
-const popupChild = document.createElement('dialog');
-popupChild.classList.add('popup');
-popupChild.setAttribute('id', 'popup');
-body.appendChild(popupChild);
-popupChild.innerHTML = `
+function init() {
+  const query = window.matchMedia('(max-width: 768px)');
+
+  if (query.matches) {
+    // --------POPUP Window Mobile-------
+
+    const body = document.querySelector('body');
+    const popupChild = document.createElement('dialog');
+    popupChild.classList.add('popup');
+    popupChild.setAttribute('id', 'popup');
+    body.appendChild(popupChild);
+    popupChild.innerHTML = `
 <div class="close"><img src="images/close.svg" alt="close-icon"></div>
 <div class="mobile-image">
   <img src="./images/popup-mobile-image1.png" alt="Snapshoot-portfolio">
@@ -40,25 +46,73 @@ popupChild.innerHTML = `
   <li>css</li>
   <li>javScript</li>
 </ul>
-<div class="popup-details">
+<p class="popup-details">
   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
-</div>
+</p>
 <div class="popup-action-btn">
   <button><a href="#">See Live</a><img src="./images/popup-live.svg" alt="Go-live"></button>
   <button><a href="https://github.com/Mujeeb4582/Portfolio">See Source</a><img src="./images/popup-github.svg" alt="Gitub-file"></button>
 </div>
 `;
 
-const popup = document.querySelector('#popup');
-const openPopup = document.querySelectorAll('.project');
-const closePopup = document.querySelector('.close');
+    const popup = document.querySelector('#popup');
+    const openPopup = document.querySelectorAll('.project');
+    const closePopup = document.querySelector('.close');
 
-openPopup.forEach((project) => {
-  project.addEventListener('click', () => {
-    popup.showModal();
-  });
-});
+    openPopup.forEach((project) => {
+      project.addEventListener('click', () => {
+        popup.showModal();
+      });
+    });
 
-closePopup.addEventListener('click', () => {
-  popup.close();
-});
+    closePopup.addEventListener('click', () => {
+      popup.close();
+    });
+  } else {
+    // ------- popup window for desktop --------
+
+    const body = document.querySelector('body');
+    const dPopupChild = document.createElement('dialog');
+    dPopupChild.classList.add('dpopup');
+    dPopupChild.setAttribute('id', 'dpopup');
+    body.appendChild(dPopupChild);
+    dPopupChild.innerHTML = `
+<div class="dclose"><img src="images/desktop-close.svg" alt="close-icon"></div>
+<div class="desktop-image">
+  <img src="./images/popup-desktop-image.svg" alt="Snapshoot-portfolio">
+</div>
+<h3 class="popup-title">Keeping track of hundreds of components</h3>
+<ul class="popup-items">
+  <li>Codekit</li>
+  <li>GitHub</li>
+  <li>javScript</li>
+  <li>Boostrap</li>
+  <li>Terminal</li>
+  <li>Codepen</li>
+  </ul>
+<p class="popup-details">
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
+</p>
+<div class="popup-action-btn">
+  <button><a href="#">See Live</a><img src="./images/popup-live.svg" alt="Go-live"></button>
+  <button><a href="https://github.com/Mujeeb4582/Portfolio">See Source</a><img src="./images/popup-github.svg" alt="Gitub-file"></button>
+</div>
+`;
+
+    const dpopup = document.querySelector('#dpopup');
+    const dopenPopup = document.querySelectorAll('.dproject');
+    const dclosePopup = document.querySelector('.dclose');
+
+    dopenPopup.forEach((dproject) => {
+      dproject.addEventListener('click', () => {
+        dpopup.showModal();
+      });
+    });
+
+    dclosePopup.addEventListener('click', () => {
+      dpopup.close();
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
