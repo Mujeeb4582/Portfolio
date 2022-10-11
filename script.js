79365deb967b0981e5edf127f7a10d82fba940d9
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-11 20:25:34 +0500

    Add the popup window code dynamicaly

--- script.js
+++ script.js
@@ -24,12 +24,39 @@ menuItems.forEach((menuItem) => {
 
 // --------POPUP Window
 
+const body = document.querySelector('body');
+const popupChild = document.createElement('section');
+body.appendChild(popupChild);
+popupChild.innerHTML = `<dialog class="popup" id="popup">
+<div class="close"><img src="images/close.svg" alt="close-icon"></div>
+<div class="mobile-image">
+  <img src="./images/popup-mobile-image1.png" alt="Snapshoot-portfolio">
+</div>
+<h3 class="popup-title">Keeping track of hundreds of components</h3>
+<ul class="popup-items">
+  <li>Ruby on rails</li>
+  <li>css</li>
+  <li>javScript</li>
+</ul>
+<div class="popup-details">
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea
+</div>
+<div class="popup-action-btn">
+  <button><a href="#">See Live</a><img src="./images/popup-live.svg" alt="Go-live"></button>
+  <button><a href="#">See Source</a><img src="./images/popup-github.svg" alt="Gitub-file"></button>
+</div>
+
+</dialog>
+`;
+
 const popup = document.querySelector('#popup');
-const openPopup = document.querySelector('.project');
+const openPopup = document.querySelectorAll('.project');
 const closePopup = document.querySelector('.close');
 
-openPopup.addEventListener('click', () => {
-  popup.showModal();
+openPopup.forEach((project) => {
+  project.addEventListener('click', () => {
+    popup.showModal();
+  });
 });
 
 closePopup.addEventListener('click', () => {
