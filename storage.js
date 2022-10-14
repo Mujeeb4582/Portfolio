61844381abc10255f3d7cdee69af31ec0b9d8dc4
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-10-14 18:37:06 +0500

    Add the localStorage features in the form

---
+++ storage.js
@@ -0,0 +1,36 @@
+const userName = document.querySelector('#user_name');
+const userEmail = document.querySelector('#email');
+const userMsg = document.querySelector('#message');
+const userForm = document.querySelector('#contact');
+const body = document.querySelector('body');
+
+const retrieveData = localStorage.getItem('info');
+
+body.onload = () => {
+  if (retrieveData) {
+    const previousData = JSON.parse(retrieveData);
+    userName.value = previousData.name;
+    userEmail.value = previousData.email;
+    userMsg.value = previousData.message;
+  }
+};
+
+document.querySelectorAll('.input').forEach((input) => {
+  input.addEventListener('input', (event) => {
+    event.preventDefault();
+
+    const userInput = {
+      name: userName.value,
+      email: userEmail.value,
+      message: userMsg.value,
+    };
+
+    // store the object in localStorage
+
+    localStorage.setItem('info', JSON.stringify(userInput));
+  });
+});
+
+userForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
