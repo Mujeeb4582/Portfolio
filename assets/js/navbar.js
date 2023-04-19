6a960447452c6e73d8c1d22e1799dcdd1c4bfda1
Mujeeb ur Rahman <mujeeburahman4582@gmail.com>
2023-04-19 13:23:13 +0500

    Add features to navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -3,6 +3,9 @@ const menuItems = document.querySelectorAll('.menu-item');
 const hamburger = document.querySelector('.hamburger');
 const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
+const profile = document.querySelector('.profile');
+const about = document.querySelector('.about');
+const contact = document.querySelector('.contact');
 
 const toggleMenu = () => {
   if (menu.classList.contains('showMenu')) {
@@ -16,6 +19,28 @@ const toggleMenu = () => {
   }
 };
 
+const navItemBackgroundColor = (
+  profileColor,
+  profileBackgroundColor,
+  aboutColor,
+  aboutBackgroundColor,
+  contactColor,
+  contactBackgroundColor,
+) => {
+  profile.style.backgroundColor = profileBackgroundColor;
+  profile.style.color = profileColor;
+  about.style.backgroundColor = aboutBackgroundColor;
+  about.style.color = aboutColor;
+  contact.style.backgroundColor = contactBackgroundColor;
+  contact.style.color = contactColor;
+};
+
 hamburger.addEventListener('click', toggleMenu);
 
 menuItems.forEach((menuItem) => menuItem.addEventListener('click', toggleMenu));
+
+profile.addEventListener('click', () => navItemBackgroundColor('white', '#36b37e', '#344563', 'white', '#344563', 'white'));
+
+about.addEventListener('click', () => navItemBackgroundColor('#344563', 'white', 'white', '#36b37e', '#344563', 'white'));
+
+contact.addEventListener('click', () => navItemBackgroundColor('#344563', 'white', '#344563', 'white', 'white', '#36b37e'));
