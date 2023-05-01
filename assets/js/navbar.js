c2abafc19accc68405d1f52647f4e6786d068635
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-05-01 23:11:47 +0500

    Add some styling effect to navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,21 +1,21 @@
-const menu = document.querySelector('.menu');
-const menuItems = document.querySelectorAll('.menu-item');
-const hamburger = document.querySelector('.hamburger');
-const closeIcon = document.querySelector('.closeIcon');
-const menuIcon = document.querySelector('.menuIcon');
-const profile = document.querySelector('.profile');
-const about = document.querySelector('.about');
-const contact = document.querySelector('.contact');
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menu-item");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+const profile = document.querySelector(".profile");
+const about = document.querySelector(".about");
+const contact = document.querySelector(".contact");
 
 const toggleMenu = () => {
-  if (menu.classList.contains('showMenu')) {
-    menu.classList.remove('showMenu');
-    closeIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
   } else {
-    menu.classList.add('showMenu');
-    closeIcon.style.display = 'block';
-    menuIcon.style.display = 'none';
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
   }
 };
 
@@ -25,7 +25,7 @@ const navItemBackgroundColor = (
   aboutColor,
   aboutBackgroundColor,
   contactColor,
-  contactBackgroundColor,
+  contactBackgroundColor
 ) => {
   profile.style.backgroundColor = profileBackgroundColor;
   profile.style.color = profileColor;
@@ -35,12 +35,53 @@ const navItemBackgroundColor = (
   contact.style.color = contactColor;
 };
 
-hamburger.addEventListener('click', toggleMenu);
+hamburger.addEventListener("click", toggleMenu);
 
-menuItems.forEach((menuItem) => menuItem.addEventListener('click', toggleMenu));
+menuItems.forEach((menuItem) => menuItem.addEventListener("click", toggleMenu));
 
-profile.addEventListener('click', () => navItemBackgroundColor('white', '#36b37e', '#344563', 'white', '#344563', 'white'));
+profile.addEventListener("click", () =>
+  navItemBackgroundColor(
+    "white",
+    "#36b37e",
+    "#344563",
+    "white",
+    "#344563",
+    "white"
+  )
+);
 
-about.addEventListener('click', () => navItemBackgroundColor('#344563', 'white', 'white', '#36b37e', '#344563', 'white'));
+about.addEventListener("click", () =>
+  navItemBackgroundColor(
+    "#344563",
+    "white",
+    "white",
+    "#36b37e",
+    "#344563",
+    "white"
+  )
+);
 
-contact.addEventListener('click', () => navItemBackgroundColor('#344563', 'white', '#344563', 'white', 'white', '#36b37e'));
+contact.addEventListener("click", () =>
+  navItemBackgroundColor(
+    "#344563",
+    "white",
+    "#344563",
+    "white",
+    "white",
+    "#36b37e"
+  )
+);
+
+const desktopHeader = document.querySelector("header nav ul");
+
+// const mobileHeader = document.querySelector("header nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    desktopHeader.classList.add("nav-border-bottom-blurred-bg");
+    // mobileHeader.classList.add("nav-border-bottom-blurred-bg");
+  } else {
+    desktopHeader.classList.remove("nav-border-bottom-blurred-bg");
+    // mobileHeader.classList.remove("nav-border-bottom-blurred-bg");
+  }
+});
