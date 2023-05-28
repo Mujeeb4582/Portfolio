cb7216e29a6a5a1598a003710f0c34c9c00c326e
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-05-28 15:05:59 +0500

    Remove the duplicate code

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -4,12 +4,10 @@ const workData = [
     title: 'Daily ToDo Task',
     projectDetail:
       'A website that lets you add list of to do task that helps you to organize your day. It lists the tasks that you need to do and allows you to mark them as complete. It uses local-storage to store the data.It was built as an introduction to web dev using ES6 and Webpack.',
-    mobileImg: './ProjectImages/List-of-to-do.gif',
-    desktopImg: './ProjectImages/List-of-to-do.gif',
+    Img: './ProjectImages/List-of-to-do.gif',
     githubIcon: './images/popup-github.svg',
     liveIcon: './images/popup-live.svg',
-    mobileTech: ['HTML', 'CSS', 'ES6 JavaScript', 'Git', 'Webpack', 'Eslint'],
-    desktopTech: ['HTML', 'CSS', 'ES6 JavaScript', 'Git', 'Webpack', 'Eslint'],
+    Tech: ['HTML', 'CSS', 'ES6 JavaScript', 'Git', 'Webpack', 'Eslint'],
     sourceLink: 'https://github.com/Mujeeb4582/To-Do-List-App',
     liveLink: 'https://mujeeb4582.github.io/To-Do-List-App/dist/',
   },
@@ -18,12 +16,10 @@ const workData = [
     title: 'BookStore CMA',
     projectDetail:
       'Bookstore CMS is an SPA made for all the book lovers. It provides an easy way to organize a collection of book, as well as progress tracking functionalities. For instance, a user can easily add a new book by providing a book title and the category. It is built using React.js.',
-    mobileImg: 'https://user-images.githubusercontent.com/109666020/221373541-9bd0ac0a-5c0f-4e19-b430-b6c81ba1f2bd.png',
-    desktopImg: 'https://user-images.githubusercontent.com/109666020/221373541-9bd0ac0a-5c0f-4e19-b430-b6c81ba1f2bd.png',
+    Img: 'https://user-images.githubusercontent.com/109666020/221373541-9bd0ac0a-5c0f-4e19-b430-b6c81ba1f2bd.png',
     githubIcon: './images/popup-github.svg',
     liveIcon: './images/popup-live.svg',
-    mobileTech: ['TailWindCSS', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git'],
-    desktopTech: ['TailWindCSS', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git'],
+    Tech: ['TailWindCSS', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git'],
     sourceLink: 'https://github.com/Mujeeb4582/Bookstore-CMA',
     liveLink: 'https://mujeeb4582.github.io/Bookstore-CMA/',
   },
@@ -32,16 +28,11 @@ const workData = [
     title: 'Hello! Sharing world DIGITAL YOUTH GLOBAL SUMMIT 2019',
     projectDetail:
       `The project I have completed is my first capstone project, which involved utilizing HTML, CSS, and JavaScript to build a functional web application. Throughout the project, I worked with these programming languages to design and develop the various elements of the application.
-
-      The final product of my capstone project is a fully functional web application that includes interactive features and user-friendly design. Throughout the development process, I applied my knowledge of HTML, CSS, and JavaScript to create the necessary components and functionality for the application.
-      
-      Overall, this was an excellent opportunity for me to apply my programming skills and gain hands-on experience in creating a web application. I am proud of the final product and excited to continue building upon my knowledge in these programming languages for future projects.`,
-    mobileImg: 'https://user-images.githubusercontent.com/109666020/221826371-cfaab064-8709-4584-bd02-6335c4aa9b24.png',
-    desktopImg: 'https://user-images.githubusercontent.com/109666020/221826371-cfaab064-8709-4584-bd02-6335c4aa9b24.png',
+      `,
+    Img: 'https://user-images.githubusercontent.com/109666020/221826371-cfaab064-8709-4584-bd02-6335c4aa9b24.png',
     githubIcon: './images/popup-github.svg',
     liveIcon: './images/popup-live.svg',
-    mobileTech: ['HTML', 'CSS', 'JavaScript', 'Git', 'ESLint'],
-    desktopTech: ['HTML', 'CSS', 'JavaScript', 'Git', 'ESLint'],
+    Tech: ['HTML', 'CSS', 'JavaScript', 'Git', 'ESLint'],
     sourceLink: 'https://github.com/Mujeeb4582/FirstCapStone',
     liveLink: 'https://mujeeb4582.github.io/FirstCapStone/',
   },
@@ -50,12 +41,10 @@ const workData = [
     title: 'Space-X',
     projectDetail:
       'This web application offers space travel services for both commercial and scientific purposes. It allows users to reserve rockets and participate in specific space missions. The application is designed to make it easy for people to access and experience the wonders of space travel.',
-    mobileImg: 'https://repository-images.githubusercontent.com/579898104/cfcbe2c9-84b6-43e8-8974-75a89a4acb61',
-    desktopImg: 'https://repository-images.githubusercontent.com/579898104/cfcbe2c9-84b6-43e8-8974-75a89a4acb61',
+    Img: 'https://repository-images.githubusercontent.com/579898104/cfcbe2c9-84b6-43e8-8974-75a89a4acb61',
     githubIcon: './images/popup-github.svg',
     liveIcon: './images/popup-live.svg',
-    mobileTech: ['TailWindCSS', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git', 'Pair-Programming'],
-    desktopTech: ['TailWindCSS', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git', 'Pair-Programming'],
+    Tech: ['TailWindCSS', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git', 'Pair-Programming'],
     sourceLink: 'https://github.com/Mujeeb4582/space-x',
     liveLink: 'https://space-traval-x.netlify.app/',
   },
@@ -64,12 +53,10 @@ const workData = [
     title: 'Air-Pollution-App',
     projectDetail:
       'This is a web application that is designed to be used on mobile devices such as smartphones or tablets. The primary purpose of the application is to retrieve data on air pollution from an API (which is likely an external data source that provides the information) and display it in a web browser. To build this application, the developers chose to use two popular frameworks called React and Redux. React is a JavaScript library for building user interfaces, while Redux is a state management library that helps manage the data flow of the application. Using these frameworks allowed the developers to build a responsive and dynamic application that is optimized for mobile devices.',
-    mobileImg: 'https://github.com/Mujeeb4582/Recipe-app/assets/109666020/572173ba-ae64-4555-aa0a-6aebf78121c5',
-    desktopImg: 'https://github.com/Mujeeb4582/Recipe-app/assets/109666020/572173ba-ae64-4555-aa0a-6aebf78121c5',
+    Img: 'https://github.com/Mujeeb4582/Recipe-app/assets/109666020/572173ba-ae64-4555-aa0a-6aebf78121c5',
     githubIcon: './images/popup-github.svg',
     liveIcon: './images/popup-live.svg',
-    mobileTech: ['BootStrap', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git', 'Pair-Programming'],
-    desktopTech: ['BootStrap', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git', 'Pair-Programming'],
+    Tech: ['BootStrap', 'ES6 JavaScript', 'React', 'Redux', 'Rest-API', 'Unit testing', 'Git', 'Pair-Programming'],
     sourceLink: 'https://github.com/Mujeeb4582/Air-Pollution-App',
     liveLink: 'https://air-pollution.onrender.com/',
   },
@@ -78,12 +65,10 @@ const workData = [
     title: 'Recipe-APP',
     projectDetail:
       'The Recipe App enables users to conveniently manage their recipes, ingredients, and inventory. Users can save ingredients, track their availability, create recipes, and generate a comprehensive shopping list that takes into account the ingredients they have and those they need for a particular recipe. Moreover, as sharing recipes is a crucial aspect of cooking, the application allows users to make their recipes public for easy access by anyone.',
-    mobileImg: 'https://github.com/Mujeeb4582/Recipe-app/assets/109666020/59f75a12-75c1-4cc6-b2b3-a14918af0395',
-    desktopImg: 'https://github.com/Mujeeb4582/Recipe-app/assets/109666020/59f75a12-75c1-4cc6-b2b3-a14918af0395',
+    Img: 'https://github.com/Mujeeb4582/Recipe-app/assets/109666020/59f75a12-75c1-4cc6-b2b3-a14918af0395',
     githubIcon: './images/popup-github.svg',
     liveIcon: './images/popup-live.svg',
-    mobileTech: ['Ruby on Rails', 'TailWindCSS', 'Rails-RSpec', 'PostgreSQL', 'Unit test', 'Integration test', 'Rubocop-rails', 'Pair-Programming'],
-    desktopTech: ['Ruby on Rails', 'TailWindCSS', 'Rails-RSpec', 'PostgreSQL', 'Unit test', 'Integration test', 'Rubocop-rails', 'Pair-Programming'],
+    Tech: ['Ruby on Rails', 'TailWindCSS', 'Rails-RSpec', 'PostgreSQL', 'Unit test', 'Integration test', 'Rubocop-rails', 'Pair-Programming'],
     sourceLink: 'https://github.com/Mujeeb4582/Recipe-app',
     liveLink: 'https://recipe-app-jk5k.onrender.com/',
   },
@@ -108,18 +93,12 @@ function dynamicpro(proidx) {
   <div onclick="closeModal()" class="dclose"><img src="images/desktop-close.svg" alt="close-icon"></div>
   <div class = "modal">
   <div onclick="closeModal()" class="close"><img src="images/close.svg" alt="close-icon"></div>
-  <div class="mobile-image">
-  <img src="${myWork.mobileImg}" alt="Snapshoot-portfolio">
-  </div>
-  <div class="desktop-image">
-  <img src="${myWork.desktopImg}" alt="Snapshoot-portfolio">
+  <div class="image">
+  <img src="${myWork.Img}" alt="Snapshoot-portfolio">
   </div>
   <div class ="modal-details">
   <h3 class="popup-title">${myWork.title}</h3>
-  <ul class="popup-items"> ${myWork.mobileTech.map(
-    (ele) => `<li>${ele}</li>`,
-  ).join('')}</ul>
-  <ul class="d-popup-items"> ${myWork.desktopTech.map(
+  <ul class="popup-items"> ${myWork.Tech.map(
     (ele) => `<li>${ele}</li>`,
   ).join('')}</ul>
   <p class="popup-details">${myWork.projectDetail}</p>
