43870cd15ebe475a62fae59660e8099cf0095274
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-06-11 12:36:48 +0500

    Add some checks to the form

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -1,20 +1,37 @@
 const form = document.querySelector('#contact');
 const email = document.getElementById('email');
+const userName = document.getElementById('user_name');
 const errorInfo = document.getElementById('error');
 
 function validateEmail() {
   let errorMessage = '';
-  const emailPattren = /^[a-z-0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z-0-9-]+(?:\.[a-z-0-9-]+)*$/;
-  if (!email.value.match(emailPattren)) {
-    errorMessage += 'The content of the email field has to be in lower case.';
+  const emailPattern = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9-]+(?:\.[a-z0-9-]+)*$/;
+
+  // Trim the input values to remove leading and trailing spaces
+  const emailValue = email.value.trim();
+  const userNameValue = userName.value.trim();
+
+  if (!emailValue.match(emailPattern)) {
+    errorMessage += 'The content of the email field has to be a valid email address.';
+  }
+
+  if (userNameValue === '') {
+    errorMessage += 'Username field cannot be empty.';
+  } else if (userNameValue.length < 3) {
+    errorMessage += 'Username should be at least 3 characters long.';
+  }
+
+  if (errorMessage !== '') {
     errorInfo.innerHTML = `
       <span style='color:red; border:1px solid red; padding: 8px;'>${errorMessage}</span>
     `;
     return false;
   }
+
   errorMessage += 'Form sent successfully';
   errorInfo.innerHTML = `
-      <span style='color:green; border:1px solid green; padding: 8px;'>${errorMessage}</span>`;
+    <span style='color:green; border:1px solid green; padding: 8px;'>${errorMessage}</span>
+  `;
   return true;
 }
 
